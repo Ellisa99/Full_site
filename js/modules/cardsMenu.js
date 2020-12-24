beafbd2cf81b37573843c2a4d44aec2cd3b27406
Ellisa99 <32750465+Ellisa99@users.noreply.github.com>
2020-12-24 13:25:49 +0300

    Update cardsMenu.js

--- js/modules/cardsMenu.js
+++ js/modules/cardsMenu.js
@@ -34,7 +34,7 @@ function cards(){
         return await res.json();
     };
     
-    getCard('http://localhost:3000/menu')
+    getCard('https://github.com/Ellisa99/Full_site/blob/master/db.json/menu')
     .then(data => {
         data.forEach(obj => new RectCards(obj.img, obj.altimg, obj.title, obj.descr, obj.price, 'card').render());
     });
